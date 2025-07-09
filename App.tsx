e59f0d1cd7807c45d964dd69939661d613947782
masanoni <jenseits.von.gut@gmail.com>
2025-07-09 17:53:51 +0900

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -221,7 +221,7 @@ const loadProjectMembers = useCallback(async () => {
   try {
     const { data, error } = await supabase
       .from('project_members')
-      .select('*,  id(email), invited_by(email)')  // invited_byも一緒に取得したい場合
+      .select('*,  user_id(email)')  // invited_byも一緒に取得したい場合
       .eq('project_id', currentProject.id);
 
     if (error) {
